9ba9c47854ed05958363b97014b707f5148ffada
lbejiuk <sergeyzarva@gmail.com>
2022-02-06 11:51:06 +0100

    Add profiles

--- go.mod
+++ go.mod
@@ -1,11 +1,11 @@
 module github.com/unix-streamdeck/api
 
-go 1.14
+go 1.17
 
 require (
 	github.com/fogleman/gg v1.3.0
-	github.com/godbus/dbus/v5 v5.0.4-0.20200513180336-df5ef3eb7cca
+	github.com/godbus/dbus/v5 v5.0.6
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
-	golang.org/x/image v0.0.0-20200801110659-972c09e46d76
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
 )
--- obj.go
+++ obj.go
@@ -3,63 +3,76 @@ package api
 import "image"
 
 type StreamDeckInfo struct {
-	Cols int `json:"cols,omitempty"`
-	Rows int `json:"rows,omitempty"`
-	IconSize int `json:"icon_size,omitempty"`
-	Page int `json:"page"`
-	Serial string `json:"serial,omitempty"`
+	Cols     int    `json:"cols,omitempty"`
+	Rows     int    `json:"rows,omitempty"`
+	IconSize int    `json:"icon_size,omitempty"`
+	Page     int    `json:"page"`
+	Serial   string `json:"serial,omitempty"`
 }
 
 type Page []Key
 
-type Deck struct {
-	Serial string `json:"serial"`
+type Profile struct {
+	Name  string `json:"name"`
 	Pages []Page `json:"pages"`
+}
+
+type DeprecatedDeck struct {
+	Serial string `json:"serial"`
+	Pages  []Page `json:"pages"`
+}
+
+type Deck struct {
+	Serial   string    `json:"serial"`
+	Profiles []Profile `json:"profiles"`
 }
 
 type Config struct {
 	Modules []string `json:"modules,omitempty"`
-	Decks []Deck `json:"decks"`
+	Decks   []Deck   `json:"decks"`
 }
 
 type DepracatedConfig struct {
 	Modules []string `json:"modules,omitempty"`
-	Pages []Page `json:"pages"`
+	Pages   []Page   `json:"pages"`
+}
+
+type DepracatedConfig_v2 struct {
+	Modules []string         `json:"modules,omitempty"`
+	Decks   []DeprecatedDeck `json:"decks"`
 }
 
 type Key struct {
-	Icon       string `json:"icon,omitempty"`
-	SwitchPage int   `json:"switch_page,omitempty"`
-	Text       string `json:"text,omitempty"`
-	TextSize	int	`json:"text_size,omitempty"`
-	TextAlignment string `json:"text_alignment,omitempty"`
-	Keybind    string `json:"keybind,omitempty"`
-	Command    string `json:"command,omitempty"`
-	Brightness int   `json:"brightness,omitempty"`
-	Url        string `json:"url,omitempty"`
-	IconHandler    string `json:"icon_handler,omitempty"`
-	KeyHandler string `json:"key_handler,omitempty"`
+	Icon              string            `json:"icon,omitempty"`
+	SwitchPage        int               `json:"switch_page,omitempty"`
+	Text              string            `json:"text,omitempty"`
+	TextSize          int               `json:"text_size,omitempty"`
+	TextAlignment     string            `json:"text_alignment,omitempty"`
+	Keybind           string            `json:"keybind,omitempty"`
+	Command           string            `json:"command,omitempty"`
+	Brightness        int               `json:"brightness,omitempty"`
+	Url               string            `json:"url,omitempty"`
+	IconHandler       string            `json:"icon_handler,omitempty"`
+	KeyHandler        string            `json:"key_handler,omitempty"`
 	IconHandlerFields map[string]string `json:"icon_handler_fields,omitempty"`
-	KeyHandlerFields map[string]string `json:"key_handler_fields,omitempty"`
-	Buff image.Image `json:"-"`
-	IconHandlerStruct IconHandler `json:"-"`
-	KeyHandlerStruct KeyHandler `json:"-"`
+	KeyHandlerFields  map[string]string `json:"key_handler_fields,omitempty"`
+	Buff              image.Image       `json:"-"`
+	IconHandlerStruct IconHandler       `json:"-"`
+	KeyHandlerStruct  KeyHandler        `json:"-"`
 }
 
 type Module struct {
-	Name string
+	Name       string
 	IconFields []Field
-	KeyFields []Field
-	IsIcon bool
-	IsKey bool
+	KeyFields  []Field
+	IsIcon     bool
+	IsKey      bool
 }
-
 
 type Field struct {
-	Title string
-	Name string
-	Type string
+	Title     string
+	Name      string
+	Type      string
 	FileTypes []string
-	Values []string
+	Values    []string
 }
-
